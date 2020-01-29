db0b789a478c76ae283b42c766739158921ed91b
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-01-29 15:45:13 -0300

    Importando UserController

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+routes.post('/users', UserController.store);
 
 export default routes;
