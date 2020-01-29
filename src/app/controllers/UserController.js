d90540e19c3efdffa5fa554ce4242860872c9828
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-01-29 20:35:22 -0300

    Adicionando método update ao controller de usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -16,6 +16,39 @@ class UserController {
     }
     return res.json({ error: 'Email already registered' });
   }
+
+  async update(req, res) {
+    const { email, oldPassword, password } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (!userExists) {
+        return res.status(401).json({ error: 'User already exists' });
+      }
+    }
+
+    if (!oldPassword || !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Wrong password' });
+    }
+
+    if (oldPassword === password) {
+      return res
+        .status(401)
+        .json({ error: 'Old and new password are the same' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
 }
 
 export default new UserController();
