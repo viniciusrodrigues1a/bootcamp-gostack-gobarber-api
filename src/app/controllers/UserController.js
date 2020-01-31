65f4f2efab226c9fcada5cfffc79384202259076
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-01-31 08:19:49 -0300

    :sparkles: valida os campos de entrada do método de criação com yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,22 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (!userExists) {
