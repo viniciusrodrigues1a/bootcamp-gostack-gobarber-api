8bdd11eac17fc136c1b6230ffb7b01a04d308e2d
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-02-04 19:40:24 -0300

    :wrench: adicionando arquivo de config do nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,11 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'f86f7e407f20c4',
+    pass: '60075b2f65186f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
